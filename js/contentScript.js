0e41855f1618196e8dfa1c0357281bf1a7d89aa8
JeremyRees <js.rees@yahoo.com>
2019-05-02 17:33:57 -0600

    Make review code syntax more forgiving
    Also validate input

--- js/contentScript.js
+++ js/contentScript.js
@@ -1,21 +1,31 @@
+if (!window.init) {
+    window.init = true;
 
-$(document).ready(function() {
-});
+    $(document).ready(function() {
+    });
 
-// May God have mercy on my soul for writing such nasty code; I'm working with what I've got.
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    var script = document.createElement('script');
-    script.textContent = `$('#cke_content').closest('.form-group').parent().find('img[alt="'+${request.imgAlt}+'"]').click().mouseover()`;
-    (document.head||document.documentElement).appendChild(script);
-    script.remove();
+    // May God have mercy on my soul for writing such nasty code; I'm working with what I've got.
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        if (request.alertMessage) {
+            alert(request.alertMessage);
+            sendResponse({success:1});
+            return;
+        }
 
-    $('.cke_editor_content').eq(1).find('iframe').contents().find('body').html('<p>'+request.review+'</p>');
-    setTimeout(function() {
-        $('.cke_editor_content').eq(1).find('iframe').contents().find('body').focus();
+        var script = document.createElement('script');
+        script.textContent = `$('#cke_content').closest('.form-group').parent().find('img[alt="'+${request.imgAlt}+'"]').click().mouseover()`;
+        (document.head||document.documentElement).appendChild(script);
+        script.remove();
+
+        $('.cke_editor_content').eq(1).find('iframe').contents().find('body').html('<p>'+request.review+'</p>');
         setTimeout(function() {
-            $('#cke_content').find('iframe').contents().find('body').focus();
+            $('.cke_editor_content').eq(1).find('iframe').contents().find('body').focus();
+            setTimeout(function() {
+                $('#cke_content').find('iframe').contents().find('body').focus();
+            }, 50);
         }, 50);
-    }, 50);
 
-    sendResponse({success:1});
-});
+        sendResponse({success:1});
+    });
+
+}
--- js/eventPage.js
+++ js/eventPage.js
@@ -12,13 +12,18 @@ var FIVE_STAR_REVIEW_COMPLIMENTS = ["Superb work", "Excellent work", "Outstandin
 var FIVE_STAR_REVIEW_BASE = "! Your article has been accepted at 5 stars!";
 
 function threeStarReview(code) {
+    var includeAddOne = code.indexOf('j') != -1;
+    var includeAddTwo = code.indexOf('k') != -1;
+
     var review = THREE_STAR_REVIEW_BASE;
-    if (code == 'j') {
-        review += THREE_STAR_REVIEW_ADD_ONE + ".";
-    } else if (code == 'k') {
-        review += THREE_STAR_REVIEW_ADD_TWO + ".";
-    } else if (code == 'jk') {
+    if (includeAddOne && includeAddTwo) {
         review += THREE_STAR_REVIEW_ADD_ONE + " and " + THREE_STAR_REVIEW_ADD_TWO + ".";
+    }
+    else if (includeAddOne) {
+        review += THREE_STAR_REVIEW_ADD_ONE + ".";
+    }
+    else if (includeAddTwo) {
+        review += THREE_STAR_REVIEW_ADD_TWO + ".";
     }
 
     return review;
@@ -26,28 +31,37 @@ function threeStarReview(code) {
 
 function fourStarReview(code) {
     var compliment = FOUR_STAR_REVIEW_COMPLIMENTS[Math.floor(Math.random() * FOUR_STAR_REVIEW_COMPLIMENTS.length)];
+    var includeAddOne = code.indexOf('j') != -1;
+    var includeAddTwo = code.indexOf('k') != -1;
+    var includeAddThree = code.indexOf('l') != -1;
 
     var review = compliment + FOUR_STAR_REVIEW_BASE;
-    if (code == 'j') {
-        review += FOUR_STAR_REVIEW_ADD_ONE + ".";
-    } else if (code == 'k') {
-        review += FOUR_STAR_REVIEW_ADD_TWO + ".";
-    } else if (code == 'l') {
-        review += FOUR_STAR_REVIEW_ADD_THREE + ".";
-    } else if (code == 'jk') {
-        review += FOUR_STAR_REVIEW_ADD_ONE + " and " + FOUR_STAR_REVIEW_ADD_TWO + ".";
-    } else if (code == 'jl') {
-        review += FOUR_STAR_REVIEW_ADD_ONE + " and " + FOUR_STAR_REVIEW_ADD_THREE + ".";
-    } else if (code == 'kl') {
-        review += FOUR_STAR_REVIEW_ADD_TWO + " and " + FOUR_STAR_REVIEW_ADD_THREE + ".";
-    } else if (code == 'jkl') {
+    if (includeAddOne && includeAddTwo && includeAddThree) {
         review += FOUR_STAR_REVIEW_ADD_ONE + ", " + FOUR_STAR_REVIEW_ADD_TWO + ", and " + FOUR_STAR_REVIEW_ADD_THREE + ".";
+    }
+    else if (includeAddOne && includeAddTwo) {
+        review += FOUR_STAR_REVIEW_ADD_ONE + " and " + FOUR_STAR_REVIEW_ADD_TWO + ".";
+    }
+    else if (includeAddOne && includeAddThree) {
+        review += FOUR_STAR_REVIEW_ADD_ONE + " and " + FOUR_STAR_REVIEW_ADD_THREE + ".";
+    }
+    else if (includeAddTwo && includeAddThree) {
+        review += FOUR_STAR_REVIEW_ADD_TWO + " and " + FOUR_STAR_REVIEW_ADD_THREE + ".";
+    }
+    else if (includeAddOne) {
+        review += FOUR_STAR_REVIEW_ADD_ONE + ".";
+    }
+    else if (includeAddTwo) {
+        review += FOUR_STAR_REVIEW_ADD_TWO + ".";
+    }
+    else if (includeAddThree) {
+        review += FOUR_STAR_REVIEW_ADD_THREE + ".";
     }
 
     return review;
 }
 
-function fiveStarReview(code) {
+function fiveStarReview() {
     var compliment = FIVE_STAR_REVIEW_COMPLIMENTS[Math.floor(Math.random() * FIVE_STAR_REVIEW_COMPLIMENTS.length)];
     var review = compliment + FIVE_STAR_REVIEW_BASE;
     return review;
@@ -55,32 +69,34 @@ function fiveStarReview(code) {
 
 // -----------------------
 //////////////////////////
-function inputReview(value) {
+function inputReview(input) {
+    if (!validateInput(input)) return;
+
     var messageData;
-    switch (value.charAt(0)) {
+    switch (input.charAt(0)) {
         case 'f':
             messageData = {
-                review: threeStarReview(value.substring(1)),
+                review: threeStarReview(input),
                 imgAlt: '3',
                 imgTitle: 'regular'
             };
             break;
         case 'd':
             messageData = {
-                review: fourStarReview(value.substring(1)),
+                review: fourStarReview(input),
                 imgAlt: '4',
                 imgTitle: 'good'
             };
             break;
         case 's':
             messageData = {
-                review: fiveStarReview(value.substring(1)),
+                review: fiveStarReview(),
                 imgAlt: '5',
                 imgTitle: 'gorgeous'
             };
             break;
         default:
-            break;
+            return;
     }
 
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -89,4 +105,35 @@ function inputReview(value) {
             console.log(response);
         });
     });
+}
+
+function validateInput(input) {
+    var alertMessage = '';
+    switch(input.charAt(0)) {
+        case 'f':
+            if (input.indexOf('j') == -1 && input.indexOf('k') == -1) {
+                alertMessage = 'The given review code is invalid. A three-star review must include one or both of the flags j or k.';
+            }
+            break;
+        case 'd':
+            if (input.indexOf('j') == -1 && input.indexOf('k') == -1 && input.indexOf('l') == -1) {
+                alertMessage = 'The given review code is invalid. A four-star review must include one or all of the flags j, k, or l.';
+            }
+            break;
+        case 's':
+            break;
+        default:
+            alertMessage = 'The given review code is invalid. The first character must be either f (3 stars), d (4 stars), or s (5 stars).';
+            break;
+    }
+
+    if (alertMessage) {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                alertMessage: alertMessage
+            });
+        });
+        return false;
+    }
+    return true;
 }
